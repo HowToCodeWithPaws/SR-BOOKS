3376ef8cc5862a847bedfbd093e60b01b4e96908
Zubareva Natalia <ndzubareva@edu.hse.ru>
2020-05-30 03:04:03 +0300

    Почему я делаю это в 3 утра, but it's fully functional

--- SR/Program.cs
+++ SR/Program.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Net.Http;
+using System.IO;
 
 namespace SR
 {
 	class Program
 	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="files"></param>
 		static void Synchronous(string[] files)
 		{
 			Stopwatch stopwatch = new Stopwatch();
@@ -19,7 +25,7 @@ namespace SR
 
 			foreach (string file in files)
 			{
-				Transliterator.ChangeFile(file); 
+				Transliterator.ChangeFile(file);
 			}
 
 			stopwatch.Stop();
@@ -29,6 +35,11 @@ namespace SR
 			Console.ForegroundColor = ConsoleColor.White;
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="files"></param>
+		/// <returns></returns>
 		static async Task Asynchronous(string[] files)
 		{
 			Stopwatch stopwatch = new Stopwatch();
@@ -46,22 +57,87 @@ namespace SR
 			Console.ForegroundColor = ConsoleColor.White;
 		}
 
-		static void SecondPart() { }
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="address"></param>
+		/// <returns></returns>
+		static async Task SecondPart(string address)
+		{
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("Second part: execution started\n");
+			Console.ForegroundColor = ConsoleColor.White;
 
+			HttpClient client = new HttpClient();
+
+			try
+			{
+				HttpResponseMessage response = await client.GetAsync(address);
+				if (response.IsSuccessStatusCode)
+				{
+					using (StreamReader reader = 
+						new StreamReader(await response.Content.ReadAsStreamAsync()))
+					{
+						using (StreamWriter writer = new StreamWriter("new_book_from_web.txt"))
+						{
+							Transliterator.Transliterate(reader, writer, "new_book_from_web");
+						}
+					}
+				}
+				else { throw new Exception("Http request failed(. Try again later."); }
+
+				stopwatch.Stop();
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine($"\nSecond part: execution finished in " +
+					$"{stopwatch.Elapsed} ({stopwatch.Elapsed:ss\\.fff} sec)\n");
+				Console.ForegroundColor = ConsoleColor.White;
+			}
+			catch (HttpRequestException)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("\nError in http request. Try again later.\n");
+				Console.ForegroundColor = ConsoleColor.White;
+			}
+			catch (ArgumentNullException)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("\nError in http request: null argument. Try again later.\n");
+				Console.ForegroundColor = ConsoleColor.White;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
 		static async Task Main()
 		{
 			string[] files = new string[] { "121-0", "1727-0", "4200-0", "58975-0",
 				"pg972", "pg3207", "pg19942", "pg27827", "pg43936" };
 
+			string fileFromWeb = "https://www.gutenberg.org/files/1342/1342-0.txt";
+
+			// 
 			do
 			{
-				Synchronous(files);
+				try
+				{
+					Synchronous(files);
 
-				await Asynchronous(files);
+					await Asynchronous(files);
 
-				SecondPart();
+					await SecondPart(fileFromWeb);
+				}
+				catch (Exception e)
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine("Something went terribly wrong:\n" + e.Message);
+					Console.ForegroundColor = ConsoleColor.White;
+				}
 
-				Console.WriteLine("\nДля повторного решения нажмите Enter, для выхода нажмите любой другой символ");
+				Console.WriteLine("\nPress Enter to execute again, press (F) anything else to exit");
 
 			} while (Console.ReadKey().Key == ConsoleKey.Enter);
 		}
--- SR/Transliterator.cs
+++ SR/Transliterator.cs
@@ -8,8 +8,14 @@ using System.Diagnostics;
 
 namespace SR
 {
+	/// <summary>
+	/// 
+	/// </summary>
 	public static class Transliterator
 	{
+		/// <summary>
+		/// 
+		/// </summary>
 		static Dictionary<char, string> letterRules = new Dictionary<char, string>
 		{
 			['A'] = "А",
@@ -66,52 +72,102 @@ namespace SR
 			['z'] = "з",
 		};
 
-		public static void ChangeFile(string filename)
-		{
-			using (var reader = new StreamReader(filename + ".txt"))
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="reader"></param>
+		/// <param name="writer"></param>
+		/// <param name="filename"></param>
+		public static void Transliterate(StreamReader reader, StreamWriter writer, string filename) {
+			bool read = true;
+			int symBefore = 0;
+			int symAfter = 0;
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+
+			while (read)
 			{
-				using (var writer = new StreamWriter("new_" + filename + ".txt"))
+				int next = reader.Read();
+
+				if (next != -1)
 				{
-					bool read = true;
-					int symBefore = 0;
-					int symAfter = 0;
-					Stopwatch stopwatch = new Stopwatch();
-					stopwatch.Start();
+					symBefore++;
+					char sym = (char)next;
 
-					while (read)
+					if (letterRules.ContainsKey(sym) && letterRules.TryGetValue(sym, out string newSym))
 					{
-						int next = reader.Read();
-
-						if (next != -1)
+						writer.Write(newSym);
+						symAfter += newSym.Length;
+					}
+					else
+					{
+						if (!char.IsLetter(sym))
 						{
-							symBefore++;
-							char sym = (char)next;
-
-							if (letterRules.ContainsKey(sym) && letterRules.TryGetValue(sym, out string newSym))
-							{
-								writer.Write(newSym);
-								symAfter += newSym.Length;
-							}
-							else
-							{
-								if (!char.IsLetter(sym))
-								{
-									writer.Write(sym);
-									symAfter++;
-								}
-							}
-						}
-						else
-						{
-							read = false;
-							stopwatch.Stop();
-
-							Console.WriteLine("File " + filename + $".txt:          " +
-								$"{symBefore} --> {symAfter}          " +
-								$"in {stopwatch.Elapsed} ({stopwatch.Elapsed:ss\\.fff} sec)");
+							writer.Write(sym);
+							symAfter++;
 						}
 					}
 				}
+				else
+				{
+					read = false;
+					stopwatch.Stop();
+
+					Console.WriteLine("File " + filename + $".txt:          " +
+						$"{symBefore} --> {symAfter}          " +
+						$"in {stopwatch.Elapsed} ({stopwatch.Elapsed:ss\\.fff} sec)");
+				}
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="filename"></param>
+		public static void ChangeFile(string filename)
+		{
+			Console.ForegroundColor = ConsoleColor.White;
+
+			try
+			{
+				using (var reader = new StreamReader(filename + ".txt"))
+				{
+					using (var writer = new StreamWriter("new_" + filename + ".txt"))
+					{
+						Transliterate(reader, writer, filename);
+					}
+				}
+			}
+			catch (FileNotFoundException)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("File not found(. Try again after fixing.");
+				Console.ForegroundColor = ConsoleColor.White;
+			}
+			catch (IOException)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Something is wrong with the file." +
+					" Try again after fixing.");
+				Console.ForegroundColor = ConsoleColor.White;
+			}
+			catch (UnauthorizedAccessException)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("The access is anauthorized(. Try again after fixing.");
+				Console.ForegroundColor = ConsoleColor.White;
+			}
+			catch (System.Security.SecurityException)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Security error(. Try again after fixing.");
+				Console.ForegroundColor = ConsoleColor.White;
+			}
+			catch (Exception ex)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Something went terribly wrong:\n " + ex.Message);
+				Console.ForegroundColor = ConsoleColor.White;
 			}
 		}
 	}
