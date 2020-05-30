a77e302784eb9f1a9efcc5b0d1256fad85c8852c
Zubareva Natalia <ndzubareva@edu.hse.ru>
2020-05-30 16:14:03 +0300

    Финальная версия с комментами

--- SR/Program.cs
+++ SR/Program.cs
@@ -7,28 +7,51 @@ using System.Diagnostics;
 using System.Net.Http;
 using System.IO;
 
+/// Ср 4.2 (?)
+/// Зубарева Наталия 
+/// БПИ199.2
+/// 30.05.2020
+
 namespace SR
 {
+	/// <summary>
+	/// Консольное приложение, резделенное на методы по задачам
+	/// в условии.
+	/// </summary>
 	class Program
 	{
 		/// <summary>
-		/// 
+		/// Метод для выполнения первой части первого задания,
+		/// то есть синхронного изменения загруженных книг.
+		/// Происходит изменение каждого файла из массива
+		/// имен файлов в цикле.
 		/// </summary>
-		/// <param name="files"></param>
+		/// <param name="files"> Параметр массива
+		/// имен обрабатываемых файлов. </param>
 		static void Synchronous(string[] files)
 		{
+			/// Таймер и его запуск.
 			Stopwatch stopwatch = new Stopwatch();
 			stopwatch.Start();
+
+			/// Настройка консоли и вывод сообщения о начале 
+			/// выполнения.
 			Console.ForegroundColor = ConsoleColor.Green;
 			Console.WriteLine("First part: synchronous execution started\n");
 			Console.ForegroundColor = ConsoleColor.White;
 
+			/// В цикле для каждого имени файла в массиве вызываем
+			/// метод изменения этого файла.
 			foreach (string file in files)
 			{
 				Transliterator.ChangeFile(file);
 			}
 
+			/// Остановка таймера, настройка внешнего вида консоли
+			/// и вывод сообщения о выполнении части задания, вывод
+			/// общего времени выполнения.
 			stopwatch.Stop();
+
 			Console.ForegroundColor = ConsoleColor.Green;
 			Console.WriteLine($"\nFirst part: synchronous execution finished in " +
 				$"{stopwatch.Elapsed} ({stopwatch.Elapsed:ss\\.fff} sec)\n");
@@ -36,21 +59,42 @@ namespace SR
 		}
 
 		/// <summary>
-		/// 
+		/// Асинхронный метод для обработки книг.
+		/// То же самое, что в первом пункте, но немного
+		/// быстрее. Что удивительно, не намного быстрее,
+		/// но, как и с потоками (один из комментариев класса
+		/// Transliterator (наболело)), не понятно, кто в этом
+		/// виноват: реализация или технические средства.
 		/// </summary>
-		/// <param name="files"></param>
+		/// <param name="files"> Массив имен обрабатываемых
+		/// файлов. </param>
 		/// <returns></returns>
 		static async Task Asynchronous(string[] files)
 		{
+			/// Таймер и его запуск.
 			Stopwatch stopwatch = new Stopwatch();
 			stopwatch.Start();
+
+			/// Настройка консоли и вывод сообщения о начале 
+			/// выполнения.
 			Console.ForegroundColor = ConsoleColor.Green;
 			Console.WriteLine("First part: asynchronous execution started\n");
 			Console.ForegroundColor = ConsoleColor.White;
 
-			await Task.WhenAll(files.Select(file => Task.Run(() => Transliterator.ChangeFile(file))));
+			/// Запуск асинхронного выполнения изменения для
+			/// каждого файла из масиива. Метод ожидает завершения
+			/// всего процесса.
+			await Task.WhenAll(files.Select(file => 
+			Task.Run(() => Transliterator.ChangeFile(file))));
 
+			/// Остановка таймера, настройка внешнего вида консоли
+			/// и вывод сообщения о выполнении части задания, вывод
+			/// общего времени выполнения. Из-за асинхронности тут 
+			/// происходит какая-то магия, временные петли, небезызвестный
+			/// парадокс близнецов и прочее. Просто смиримся, что время
+			/// в асинхронности работает не так, как мы привыкли.
 			stopwatch.Stop();
+
 			Console.ForegroundColor = ConsoleColor.Green;
 			Console.WriteLine($"\nFirst part: asynchronous execution finished in " +
 				$"{stopwatch.Elapsed} ({stopwatch.Elapsed:ss\\.fff} sec)\n");
@@ -58,14 +102,26 @@ namespace SR
 		}
 
 		/// <summary>
-		/// 
+		/// Метод для выполнения второй части задания, то есть
+		/// обработки книги, получаемой по веб-запросу.
+		/// Мне не хотелось сохранять "Гордость и предубеждение",
+		/// потому что это тяжело, поэтому происходит некоторая
+		/// гимнастика с потоками.
 		/// </summary>
-		/// <param name="address"></param>
+		/// <param name="address"> Параметр адреса,
+		/// по которому получается файл. </param>
 		/// <returns></returns>
 		static async Task SecondPart(string address)
 		{
+			/// Таймер и его запуск. Здесь он считает
+			/// в том числе время веб-запроса, поэтому
+			/// время отличается от времени, выводимого
+			/// конкретно про обработку книги.
 			Stopwatch stopwatch = new Stopwatch();
 			stopwatch.Start();
+
+			/// Настройка консоли и вывод сообщения о начале 
+			/// выполнения.
 			Console.ForegroundColor = ConsoleColor.Green;
 			Console.WriteLine("Second part: execution started\n");
 			Console.ForegroundColor = ConsoleColor.White;
@@ -74,7 +130,10 @@ namespace SR
 
 			try
 			{
+				/// Производится web запрос, что это, кто это, почему,
+				/// откуда, ладно, самообразование, че уж там.
 				HttpResponseMessage response = await client.GetAsync(address);
+
 				if (response.IsSuccessStatusCode)
 				{
 					using (StreamReader reader = 
@@ -82,13 +141,21 @@ namespace SR
 					{
 						using (StreamWriter writer = new StreamWriter("new_book_from_web.txt"))
 						{
+							/// Вызов метода транслитерации по созданным потокам.
 							Transliterator.Transliterate(reader, writer, "new_book_from_web");
 						}
 					}
 				}
 				else { throw new Exception("Http request failed(. Try again later."); }
 
+				/// На всякий случай закроем руками...
+				response.Dispose();
+
+				/// Остановка таймера, настройка внешнего вида консоли
+				/// и вывод сообщения о выполнении части задания, вывод
+				/// общего времени выполнения.
 				stopwatch.Stop();
+
 				Console.ForegroundColor = ConsoleColor.Green;
 				Console.WriteLine($"\nSecond part: execution finished in " +
 					$"{stopwatch.Elapsed} ({stopwatch.Elapsed:ss\\.fff} sec)\n");
@@ -106,28 +173,65 @@ namespace SR
 				Console.WriteLine("\nError in http request: null argument. Try again later.\n");
 				Console.ForegroundColor = ConsoleColor.White;
 			}
+			catch (FileNotFoundException)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("File not found(. Try again after fixing.");
+				Console.ForegroundColor = ConsoleColor.White;
+			}
+			catch (IOException)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Something is wrong with the file." +
+					" Try again after fixing.");
+				Console.ForegroundColor = ConsoleColor.White;
+			}
+			catch (UnauthorizedAccessException)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("The access is anauthorized(. Try again after fixing.");
+				Console.ForegroundColor = ConsoleColor.White;
+			}
+			catch (System.Security.SecurityException)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Security error(. Try again after fixing.");
+				Console.ForegroundColor = ConsoleColor.White;
+			}
+			catch (Exception ex)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Something went terribly wrong:\n " + ex.Message);
+				Console.ForegroundColor = ConsoleColor.White;
+			}
 		}
 
 		/// <summary>
-		/// 
+		/// Точка входа, метод, последовательно запускающий методы для
+		/// решения всех частей задания. Здесь также осуществляется цикл
+		/// повтора решения.
 		/// </summary>
 		/// <returns></returns>
 		static async Task Main()
 		{
+			/// Строковые переменные для хранения названий и адресов книг.
 			string[] files = new string[] { "121-0", "1727-0", "4200-0", "58975-0",
 				"pg972", "pg3207", "pg19942", "pg27827", "pg43936" };
 
 			string fileFromWeb = "https://www.gutenberg.org/files/1342/1342-0.txt";
 
-			// 
+			/// Цикл повтора решения.
 			do
 			{
 				try
 				{
+					/// Вызов метода синхронной обработки книг.
 					Synchronous(files);
 
+					/// Вызов метода асинхронной обработки книг.
 					await Asynchronous(files);
 
+					/// Вызов метода получения книги по web запросу.
 					await SecondPart(fileFromWeb);
 				}
 				catch (Exception e)
--- SR/Transliterator.cs
+++ SR/Transliterator.cs
@@ -9,12 +9,23 @@ using System.Diagnostics;
 namespace SR
 {
 	/// <summary>
-	/// 
+	/// Статический класс для преобразования книг по правилам, описанным
+	/// в условии. Здесь есть словарь, метод, создающий потоки,
+	/// и метод, преобразующий файл, полученный из потока.
+	/// Альтернативно вместо использования файловых потоков можно
+	/// было использовать обычный File.ReadAllText, мне показалось,
+	/// потоки должны быть эффективнее по времени, но моя программа 
+	/// работает медленнее, чем у коллег, правда является ли причиной
+	/// этого потоковая реализация или медленный ноутбук ¯\_(ツ)_/¯. 
 	/// </summary>
 	public static class Transliterator
 	{
 		/// <summary>
-		/// 
+		/// Словарь с правилами перевода латинских букв в кириллицу.
+		/// Было выбрано переводить Y в Й а не в Ы, потому что.
+		/// Также здесь учтены прописные буквы, хотя можно было делать
+		/// проверки на содержание symbol.ToUpper, находить соответствие
+		/// и приводить в lowercase.
 		/// </summary>
 		static Dictionary<char, string> letterRules = new Dictionary<char, string>
 		{
@@ -73,18 +84,36 @@ namespace SR
 		};
 
 		/// <summary>
-		/// 
+		/// Метод, реализующий запись нового файла, полученного
+		/// транслитерацией исходного, также здесь засекается время
+		/// и выводится сообщение о количестве символов и времени 
+		/// обработки файла. Принимает потоки, в которых
+		/// считывается старый и записывается новый файл, а также
+		/// имя файла, чтобы вывести сообшение.
 		/// </summary>
-		/// <param name="reader"></param>
-		/// <param name="writer"></param>
-		/// <param name="filename"></param>
-		public static void Transliterate(StreamReader reader, StreamWriter writer, string filename) {
+		/// <param name="reader"> Поток для считывания начального файла. </param>
+		/// <param name="writer"> Поток для записи измененного файла. </param>
+		/// <param name="filename"> Имя файла. </param>
+		public static void Transliterate(StreamReader reader,
+			StreamWriter writer, string filename)
+		{
+			/// Переменные для флага состояния считывания,
+			/// количества символов до и после преобразования.
 			bool read = true;
 			int symBefore = 0;
 			int symAfter = 0;
+
+			/// Таймер и его запуск для измерения времени выполнения.
+			/// Стоит заметить, что наверное более правильно было
+			/// бы для измерения времени отдельно считывать файл,
+			/// осуществлять преобразование и записывать новый, и 
+			/// измерять каждый промежуток отдельно, но здесь эти
+			/// этапы очень тесно связаны, так что таймер замеряет сразу все.
 			Stopwatch stopwatch = new Stopwatch();
 			stopwatch.Start();
 
+			/// Считывание и запись осуществляются по одному символу
+			/// до тех пор, пока файл не закончится.
 			while (read)
 			{
 				int next = reader.Read();
@@ -94,7 +123,11 @@ namespace SR
 					symBefore++;
 					char sym = (char)next;
 
-					if (letterRules.ContainsKey(sym) && letterRules.TryGetValue(sym, out string newSym))
+					/// Если словарь содержит символ, то символ заменяется, иначе,
+					/// если это не буквенный символ, он записыавется без изменений,
+					/// иначе пропускается.
+					if (letterRules.ContainsKey(sym) && 
+						letterRules.TryGetValue(sym, out string newSym))
 					{
 						writer.Write(newSym);
 						symAfter += newSym.Length;
@@ -110,6 +143,9 @@ namespace SR
 				}
 				else
 				{
+					/// Когда файл кончается, чтение завершается,
+					/// таймер останавливается и выводится сообщение
+					/// по обработке книги.
 					read = false;
 					stopwatch.Stop();
 
@@ -121,19 +157,19 @@ namespace SR
 		}
 
 		/// <summary>
-		/// 
+		/// Метод для создания потоков, вызова метода транслитерации и 
+		/// поимки файловых исключений.
 		/// </summary>
 		/// <param name="filename"></param>
 		public static void ChangeFile(string filename)
 		{
-			Console.ForegroundColor = ConsoleColor.White;
-
 			try
 			{
 				using (var reader = new StreamReader(filename + ".txt"))
 				{
 					using (var writer = new StreamWriter("new_" + filename + ".txt"))
 					{
+						/// Вызов метода транслитерации по созданным потокам.
 						Transliterate(reader, writer, filename);
 					}
 				}
